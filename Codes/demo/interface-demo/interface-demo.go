b5f4dd17aee1d89e0f992a490512757dcbdcb5d7
mrb503n <iangliqiange956@163.com>
2022-07-30 19:27:21 +0800

    clean empty map[string]interface items

--- Codes/demo/interface-demo/interface-demo.go
+++ Codes/demo/interface-demo/interface-demo.go
@@ -1,51 +1,121 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"golang.org/x/crypto/bcrypt"
-	"math"
+	"reflect"
 )
 
-type geometry interface {
-	area() float64
-	perim() float64
-}
-
-type rect struct {
-	width, height float64
-}
-type circle struct {
-	radius float64
-}
-
-func (r rect) area() float64 {
-	return r.width * r.height
-}
-func (r rect) perim() float64 {
-	return 2*r.width + 2*r.height
-}
-
-func (c circle) area() float64 {
-	return math.Pi * c.radius * c.radius
-}
-func (c circle) perim() float64 {
-	return 2 * math.Pi * c.radius
-}
-
-func measure(g geometry) {
-	fmt.Println(g)
-	fmt.Println(g.area())
-	fmt.Println(g.perim())
+func removeItem(m map[string]interface{}) map[string]interface{} {
+	for k, v := range m {
+		vv := reflect.ValueOf(v)
+		switch v.(type) {
+		case int:
+			if vv.IsZero() {
+				delete(m, k)
+			}
+		case bool:
+			if vv.Bool() == false {
+				delete(m, k)
+			}
+		case string:
+			if vv.String() == "" {
+				delete(m, k)
+			}
+		}
+		if !vv.IsValid() {
+			delete(m, k)
+		}
+		if vv.Kind() == reflect.Slice {
+			if vv.Len() == 0 {
+				delete(m, k)
+			} else {
+				x := []map[string]interface{}{}
+				for i := 0; i < vv.Len(); i++ {
+					vvv := reflect.ValueOf(vv.Index(i))
+					if vvv.Kind() == reflect.Struct {
+						v3 := removeItem(vv.Index(i).Interface().(map[string]interface{}))
+						x = append(x, v3)
+					}
+				}
+				m[k] = x
+			}
+		}
+		if vv.Kind() == reflect.Map {
+			v2 := removeItem(v.(map[string]interface{}))
+			m[k] = v2
+		}
+	}
+	m2 := m
+	return m2
 }
 
 func main() {
-	r := rect{width: 3, height: 4}
-	c := circle{radius: 5}
-
-	measure(r)
-	measure(c)
-
-	fmt.Println("#####")
-	bs, _ := bcrypt.GenerateFromPassword([]byte("fxjl5OsgtXq2X3V2JV"), 10)
-	fmt.Println(string(bs))
+	m := map[string]interface{}{
+		"string_foo":   "foo",
+		"string_empty": "",
+		"int_zero":     0,
+		"int":          1,
+		"bool_false":   false,
+		"bool_true":    true,
+		"nil":          nil,
+		"array_empty":  []string{},
+		"array_maps": []map[string]interface{}{
+			{
+				"string_foo":   "foo",
+				"string_empty": "",
+				"int_zero":     0,
+				"int":          1,
+				"bool_false":   false,
+				"bool_true":    true,
+				"nil":          nil,
+				"array":        []string{},
+			},
+			{
+				"string_foo":   "foo",
+				"string_empty": "",
+				"int_zero":     0,
+				"int":          1,
+				"bool_false":   false,
+				"bool_true":    true,
+				"nil":          nil,
+				"array":        []string{},
+			},
+		},
+		"map": map[string]interface{}{
+			"string_foo":   "foo",
+			"string_empty": "",
+			"int_zero":     0,
+			"int":          1,
+			"bool_false":   false,
+			"bool_true":    true,
+			"nil":          nil,
+			"array_empty":  []string{},
+			"array_maps": []map[string]interface{}{
+				{
+					"string_foo":   "foo",
+					"string_empty": "",
+					"int_zero":     0,
+					"int":          1,
+					"bool_false":   false,
+					"bool_true":    true,
+					"nil":          nil,
+					"array":        []string{},
+				},
+				{
+					"string_foo":   "foo",
+					"string_empty": "",
+					"int_zero":     0,
+					"int":          1,
+					"bool_false":   false,
+					"bool_true":    true,
+					"nil":          nil,
+					"array":        []string{},
+				},
+			},
+		},
+	}
+	m2 := removeItem(m)
+	data, _ := json.Marshal(m2)
+	fmt.Println(string(data))
 }
