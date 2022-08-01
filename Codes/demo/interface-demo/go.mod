9b6749e01b70614fd2bab8b95328e01e032d12d5
mrb503n <iangliqiange956@163.com>
2022-08-02 03:06:11 +0800

    clean empty map[string]interface items

--- Codes/demo/interface-demo/go.mod
+++ Codes/demo/interface-demo/go.mod
@@ -2,7 +2,4 @@ module interface-demo
 
 go 1.16
 
-require (
-	github.com/abbot/go-http-auth v0.4.0
-	golang.org/x/crypto v0.0.0-20200115085410-6d4e4cb37c7d // indirect
-)
+require gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
--- Codes/demo/interface-demo/interface-demo.go
+++ Codes/demo/interface-demo/interface-demo.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"gopkg.in/yaml.v3"
 	"reflect"
 )
 
@@ -10,7 +11,7 @@ func removeItem(m map[string]interface{}) map[string]interface{} {
 	for k, v := range m {
 		vv := reflect.ValueOf(v)
 		switch v.(type) {
-		case int:
+		case int, float32, float64:
 			if vv.IsZero() {
 				delete(m, k)
 			}
@@ -30,20 +31,45 @@ func removeItem(m map[string]interface{}) map[string]interface{} {
 			if vv.Len() == 0 {
 				delete(m, k)
 			} else {
+				var isMap bool
 				x := []map[string]interface{}{}
 				for i := 0; i < vv.Len(); i++ {
 					vvv := reflect.ValueOf(vv.Index(i))
-					if vvv.Kind() == reflect.Struct {
+					fmt.Println(vvv)
+					if vvv.Kind() == reflect.String {
+					} else if vvv.Kind() == reflect.Int {
+					} else if vvv.Kind() == reflect.Float32 {
+					} else if vvv.Kind() == reflect.Float64 {
+					} else if vvv.Kind() == reflect.Bool {
+					} else if vvv.Kind() == reflect.Struct {
+						isMap = true
+						v3 := removeItem(vv.Index(i).Interface().(map[string]interface{}))
+						x = append(x, v3)
+					} else if vvv.Kind() == reflect.Map {
+						isMap = true
 						v3 := removeItem(vv.Index(i).Interface().(map[string]interface{}))
 						x = append(x, v3)
 					}
 				}
-				m[k] = x
+				if isMap {
+					m[k] = x
+				}
 			}
 		}
-		if vv.Kind() == reflect.Map {
+		if vv.Kind() == reflect.Struct {
 			v2 := removeItem(v.(map[string]interface{}))
-			m[k] = v2
+			if len(v2) == 0 {
+				delete(m, k)
+			} else {
+				m[k] = v2
+			}
+		} else if vv.Kind() == reflect.Map {
+			v2 := removeItem(v.(map[string]interface{}))
+			if len(v2) == 0 {
+				delete(m, k)
+			} else {
+				m[k] = v2
+			}
 		}
 	}
 	m2 := m
@@ -59,7 +85,7 @@ func main() {
 		"bool_false":   false,
 		"bool_true":    true,
 		"nil":          nil,
-		"array_empty":  []string{},
+		"array_empty":  []string{"a"},
 		"array_maps": []map[string]interface{}{
 			{
 				"string_foo":   "foo",
@@ -118,4 +144,65 @@ func main() {
 	m2 := removeItem(m)
 	data, _ := json.Marshal(m2)
 	fmt.Println(string(data))
+
+	strYaml := `
+deployName: tp1-spring-demo
+relatedPackage: tp1-spring-demo
+deployImageTag: ""
+deployLabels: {}
+deploySessionAffinityTimeoutSeconds: 0
+deployNodePorts: []
+deployLocalPorts:
+  - port: 9000
+    protocol: http
+    ingress:
+      domainName: demo.test-project1.local
+      pathPrefix: /spring/
+deployReplicas: 1
+hpaConfig:
+  maxReplicas: 0
+  memoryAverageValue: ""
+  memoryAverageRequestPercent: 0
+  cpuAverageValue: ""
+  cpuAverageRequestPercent: 0
+deployEnvs:
+  - JAVA_OPTS=-Xms256m -Xmx256m
+deployCommand: sh -c "java -jar example.smallest-0.0.1-SNAPSHOT.war 2>&1 | sed \"s/^/[$(hostname)] /\" | tee -a /tp1-spring-demo/logs/tp1-spring-demo.logs"
+deployCmd: []
+deployResources:
+  memoryRequest: 10Mi
+  memoryLimit: 250Mi
+  cpuRequest: "0.05"
+  cpuLimit: "0.25"
+deployVolumes:
+  - pathInPod: /tp1-spring-demo/logs
+    pathInPv: tp1-spring-demo/logs
+    pvc: ""
+deployHealthCheck:
+  checkPort: 0
+  httpGet:
+      path: /
+      port: 9000
+      httpHeaders: []
+  readinessDelaySeconds: 15
+  readinessPeriodSeconds: 5
+  livenessDelaySeconds: 150
+  livenessPeriodSeconds: 30
+dependServices: []
+hostAliases: []
+securityContext:
+  runAsUser: 0
+  runAsGroup: 0
+deployConfigSettings: []
+`
+	var mapYaml map[string]interface{}
+	err := yaml.Unmarshal([]byte(strYaml), &mapYaml)
+	if err != nil {
+		fmt.Println("[ERROR]", err.Error())
+		return
+	}
+	mapYaml = removeItem(mapYaml)
+	bs, _ := yaml.Marshal(mapYaml)
+	fmt.Println(string(bs))
+
 }
