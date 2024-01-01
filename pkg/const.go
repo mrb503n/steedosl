499f92576af82ba7a4e78c69a499bd793d4e41fd
mrb503n <iangliqiange956@163.com>
2024-01-02 01:43:35 +0800

    dory-core:v1.7.0-rc.3

--- pkg/const.go
+++ pkg/const.go
@@ -4,7 +4,7 @@ import "embed"
 
 const (
 	VersionDoryCtl       = "v0.8.0"
-	VersionDoryCore      = "v1.7.0-rc.2"
+	VersionDoryCore      = "v1.7.0-rc.3"
 	VersionDoryDashboard = "v1.6.4"
 	BaseCmdName          = "doryctl"
 	ConfigDirDefault     = ".doryctl"
